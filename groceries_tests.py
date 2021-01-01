812e0c5a4e55742f3edf619c81831216c558bdda
Angel <angelmg58@gmail.com>
2020-12-31 23:42:46 -0800

    Ran unit tests/created kivy app file

--- groceries_tests.py
+++ groceries_tests.py
@@ -1,45 +1,50 @@
-import groceries.py
+import groceries
 
 if __name__ == '__main__':
-    service = main()
+    service = groceries.main()
     # to create initial doc
     #doc = create_document(service)
     doc = "1fzSVQAaERQ938fgjDosOHjsYG6Z9fJltzHMCjTPRMtA"
 
     # INSERT TEXT TEST
-    start_h, end_h = get_text_range_idx(service, doc, "Health")
+    start_h, end_h = groceries.get_text_range_idx(service, doc, "Health", True)
     #print('end_h: '+str(end_h))
-    insert_text(service, doc, end_h, 'floss')
+    groceries.insert_text(service, doc, end_h, 'floss', groceries.yellow, True)
 
-    start_c, end_c = get_text_range_idx(service, doc, "Carne")
+    start_c, end_c = groceries.get_text_range_idx(service, doc, "Carne", True)
     #print('end_c: '+str(end_c))
-    insert_text(service, doc, end_c, 'chicken (3lb)')
+    groceries.insert_text(service, doc, end_c, 'chicken (3lb)', groceries.yellow
+    , True)
 
-    start_cagain, end_cagain = get_text_range_idx(service, doc, "Carne")
-    insert_text(service, doc, end_cagain, 'ground beef')
+    start_cagain, end_cagain = groceries.get_text_range_idx(service, doc,
+    "Carne", True)
+    groceries.insert_text(service, doc, end_cagain, 'ground beef',
+    groceries.yellow, True)
 
-    start_o, end_o = get_text_range_idx(service, doc, "Hot stuff")
-    insert_text(service, doc, end_o, 'chicken wings')
+    start_o, end_o = groceries.get_text_range_idx(service, doc, "Hot stuff",
+    True)
+    groceries.insert_text(service, doc, end_o, 'chicken wings',
+    groceries.yellow, True)
 
     # CSV TEST
-    Meals = pd.read_csv('Meals Table.csv', index_col='id')
-    Ingredients = pd.read_csv('Ingredients Table.csv')
+    # Meals = pd.read_csv('Meals Table.csv', index_col='id')
+    # Ingredients = pd.read_csv('Ingredients Table.csv')
+    #
+    # curr_id = 1;
+    #
+    # # update this_time, last_time variables
+    # if Meals.loc[curr_id, 'this_time'] == 1:
+    #     Meals.loc[curr_id, 'last_time'] = 1
+    # else:
+    #     Meals.loc[curr_id, 'this_time'] = 1
+    #     Meals.loc[curr_id, 'last_time'] = 0
+    #
+    # # get meal name corresponding to the specified id
+    # Meal_name = Meals.loc[curr_id, 'name']
+    #
+    # # loops through rows with specified id and prints out the ingredient name/
+    # # section
+    # for index,row in Ingredients[Ingredients['id']==curr_id].iterrows():
+    #     print('Name: '+row['name']+' Section: '+row['section'])
 
-    curr_id = 1;
-
-    # update this_time, last_time variables
-    if Meals.loc[curr_id, 'this_time'] == 1:
-        Meals.loc[curr_id, 'last_time'] = 1
-    else:
-        Meals.loc[curr_id, 'this_time'] = 1
-        Meals.loc[curr_id, 'last_time'] = 0
-
-    # get meal name corresponding to the specified id
-    Meal_name = Meals.loc[curr_id, 'name']
-
-    # loops through rows with specified id and prints out the ingredient name/
-    # section
-    for index,row in Ingredients[Ingredients['id']==curr_id].iterrows():
-        print('Name: '+row['name']+' Section: '+row['section'])
-
-    Meals.to_csv('Meals Table.csv')
+    # Meals.to_csv('Meals Table.csv')
