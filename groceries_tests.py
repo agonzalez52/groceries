e6ddfa514947535e69b8bdef3608a3314253f4c9
Angel <angelmg58@gmail.com>
2021-02-20 21:13:50 -0800

    Added g-sheet support to tests Need to add support to main file

--- groceries_tests.py
+++ groceries_tests.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     #doc = create_document(service)
     doc = "1fzSVQAaERQ938fgjDosOHjsYG6Z9fJltzHMCjTPRMtA"
 
-    # INSERT TEXT TEST
+    # INSERT TEXT TO DOC TEST
     # start_h, end_h = groceries.get_text_range_idx(service, doc, "Health", True)
     # #print('end_h: '+str(end_h))
     # groceries.insert_text(service, doc, end_h, 'floss', groceries.yellow, True)
@@ -29,43 +29,39 @@ if __name__ == '__main__':
     # groceries.insert_text(service, doc, end_o, 'chicken wings',
     # groceries.yellow, True)
 
-    # CSV TEST
+    # Google sheet TEST
     Meals_old = pd.read_csv('Meals Table.csv', index_col='id')
     Ingredients_old = pd.read_csv('Ingredients Table.csv')
 
     Meals_data = groceries.pull_sheet_data(sheet_service, 'Meals')
     Meals = pd.DataFrame(Meals_data[1:], columns=Meals_data[0])
     Meals = Meals.set_index('id')
-    #print(Meals)
     Ingredients_data = groceries.pull_sheet_data(sheet_service, 'Ingredients')
     Ingredients = pd.DataFrame(Ingredients_data[1:], columns=Ingredients_data[0])
 
-    ids = [1,15,21]
+    # TEST updating week and looping through meal ingredients
+    ids = [2,16,22]
     for id in ids:
         # get meal name corresponding to the specified id
         Meal_name = Meals.loc[str(id), 'name']
 
+        # update date
         Meals.loc[str(id), 'week'] = '12-12-21'
-
 
         # loops through rows with specified id and prints out the ingredient name/
         # section
         for index,row in Ingredients[Ingredients['id']==str(id)].iterrows():
             print('Name: '+row['name']+' Section: '+row['section'])
 
-    list = [["valuea1"], ["valuea2"], ["valuea3"]]
-    #print(list)
-    #print(Meals.loc[:,'week'].values.tolist())
+    # TEST getting only the 'week' column
     date_values = np.reshape(Meals.loc[:,'week'].values.tolist(), (len(Meals.index), 1))
-    # print(Meals.loc[:,'week'])
-    # print('------------------')
-    date_list = Meals.loc[:,'week'].values.tolist()
-    date_list = [date_list[i:i+1] for i in range(0, len(date_list), 1)]
+    date_length = len(Meals.index)
 
+    # TEST writing to google sheet - updating date
     response_date = sheet_service.spreadsheets().values().update(
         spreadsheetId=groceries.sheet_id,
         valueInputOption='USER_ENTERED',
-        range='Meals!D2:',
+        range='Meals!D2:D{}'.format(date_length+1),
         body=dict(
             majorDimension='ROWS',
             values=date_values.tolist())
