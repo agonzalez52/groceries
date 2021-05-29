23189b19070d19191acc85023080a357b5adb80b
Angel <agonzalez@steadyapp.com>
2021-05-29 15:08:00 -0700

    fixed greeting bug

--- groceries.py
+++ groceries.py
@@ -6,30 +6,28 @@
 
 import sys
 sys.path.append('helper_functions')
-import groceries_funcs as grc
+import groceries_funcs as gf
 import gdocs_funcs as gfuncs
 from datetime import date
 from datetime import datetime
 
 def greeting():
     curr_hour = datetime.now().hour
-    if curr_hour >= 0 or curr_hour < 12:
+    if curr_hour >= 0 and curr_hour < 12:
         print('\n\nGood Morning!\n\n')
-    elif cur_hour >=12 or curr_hour < 17:
+    elif curr_hour >=12 and curr_hour < 17:
         print('\n\nGood Afternoon!\n\n')
     else:
         print('\n\nGood Evening!\n\n')
 
 if __name__ == '__main__':
-    doc_service, sheet_service = gfuncs.build_services()
-    # to create initial doc
-    #doc = gfuncs.create_document(service)
-    # for existing doc
     greeting()
 
-    start_date = date(1980, 1, 4)
-    grc.update_grocery_list([27,31,7], doc_service, sheet_service, start_date)
-    #grc.make_reminders([12,41,13,24,37,1],doc_service,sheet_service,start_date)
+    doc_service, sheet_service = gfuncs.build_services()
+
+    start_date = date(2021, 6, 7)
+    gf.update_grocery_list([41,17,51,25,11,50], doc_service, sheet_service, start_date)
+    #gf.make_reminders([12,41,13,24,37,1],doc_service,sheet_service,start_date)
 
 
     print('\n\ndone =) \"Have a lovely day!\"\n\n')
