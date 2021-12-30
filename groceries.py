820ff38d7404654b36f7183c4f110b3fda31400c
Angel <agonzalez@steadyapp.com>
2021-12-30 15:58:47 -0800

    wip stuck on decode error when creating message with attachment

--- groceries.py
+++ groceries.py
@@ -27,18 +27,18 @@ if __name__ == '__main__':
     '''
     STEP 1: UNCOMMENT CORRECT IDS FOR GOOGLE DOC AND SHEET
     '''
-    doc_id = "1fzSVQAaERQ938fgjDosOHjsYG6Z9fJltzHMCjTPRMtA"
-    sheet_id = "1a4cOzCh81sp19dl3Oww3BkHmRcxAZcigq0Z5cHah0LU"
+    # doc_id = "1fzSVQAaERQ938fgjDosOHjsYG6Z9fJltzHMCjTPRMtA"
+    # sheet_id = "1a4cOzCh81sp19dl3Oww3BkHmRcxAZcigq0Z5cHah0LU"
 
-    # doc_id = "13NY4wB-BJ-FasWhN7DaM8rIf7l1RRKgXiK-a-ECIeKs" # Test sheet
-    # sheet_id = "1xd5yKYL3Ri5TWH17hIMApD4C7fOPcRHr2PK-63AsA_E" # Test sheet
+    doc_id = "13NY4wB-BJ-FasWhN7DaM8rIf7l1RRKgXiK-a-ECIeKs" # Test sheet
+    sheet_id = "1xd5yKYL3Ri5TWH17hIMApD4C7fOPcRHr2PK-63AsA_E" # Test sheet
 
     angel_calendar_id = 'angelmg58@gmail.com'
 
     '''
     STEP 2: SET THESE VARIABLES
     '''
-    meal_week = date(2021, 12, 27)
+    meal_week = date(1975, 3, 5)
     meal_ids = [3]
     font_color = fc.COLOR_RED
 
@@ -47,13 +47,15 @@ if __name__ == '__main__':
         $ python groceries.py
     '''
 
-    doc_service, sheet_service, calendar_service = gdocs.build_services()
+    doc_service, sheet_service, calendar_service, mail_service = gdocs.build_services()
     gdoc = gdocs.GoogleDoc(doc_id, doc_service)
     gsheet = gdocs.GoogleSheet(sheet_id, sheet_service)
     gcalendar = gdocs.GoogleCalendar(angel_calendar_id, calendar_service)
+    gmail = gdocs.GoogleMail(mail_service)
+    gapi = gdocs.GoogleAPI(gdoc, gsheet, gcalendar, gmail)
 
     meal_batch = grc.MealBatch(meal_week, meal_ids)
-    gdoc.set_font_color(font_color)
-    grc.update_grocery_list(meal_batch, gdoc, gsheet, gcalendar, reminders_only=0)
+    gapi.gdoc.set_font_color(font_color)
+    grc.update_grocery_list(meal_batch, gapi, reminders_only=0)
 
     print('\n\ndone =) \"Have a lovely day!\"\n\n')
